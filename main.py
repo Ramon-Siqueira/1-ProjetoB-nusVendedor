4ee8036ff79c7530d6e50f6b617083d1cc30225d
Ramon Siqueira <ramonsiqueira80@gmail.com>
2022-05-04 12:42:51 -0300

    Envio de SMS

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from twilio.rest import Client
+
+# Se for maior enviar sms com nome, mês e vendas do vendedor
+account_sid = "AC6e4954a1bf2fbe571b8a452f073255e4"
+auth_token  = "e1f9be70311464b5085d0830439c609a"
+client = Client(account_sid, auth_token)
+
 
 # lógica - Passo a passo para a solução
 
@@ -6,12 +13,15 @@ import pandas as pd
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
 for mes in lista_meses:
-    print(mes)
     tabela_vendas = pd.read_excel(f'{mes}.xlsx')
-
-print(tabela_vendas)
+    if (tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês de {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        message = client.messages.create(
+            to="+5511930878654", 
+            from_="+19705389207",
+            body=f'No mês de {mes} alguém bateu a meta. Vendedor: {vendedor}, Vendas: {vendas}')
+        print(message.sid)
 # Para cada um arquivo:
-
 # Verificar se algum valor na coluna de vendas é > que 55 mil
-
-# Se for maior enviar sms com nome, mês e vendas do vendedor
